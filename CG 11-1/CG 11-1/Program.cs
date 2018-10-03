d9b8ac6b9e91e27521095861bd9d6962d7e65a43
Erin Hogan <hogan10@charter.net>
2018-10-02 23:19:51 -0500

    CG 11-1 homework

---
+++ CG 11-1/CG 11-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CG_11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create student and use this to call your student class 
+            Student student = new Student();
+            Console.WriteLine("Please press enter to find out the names of students that have more than 6 quiz scores.");
+            Console.ReadLine();
+
+            //write the student names to the console and add that this student or students has more than 6 quiz scores. 
+            Console.WriteLine(student.StudentNames() + " " + "has more than 6 quiz scores.");
+        
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CG 11-1/CG 11-1/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace CG_11_1
+{
+
+    //create a student class
+
+    class Student
+
+    {
+        //create an array to store the text file and return it
+
+        public string[] StudentData { get; set; }
+        public string Name { get; set; }
+
+        //create a method to list out student names if they have more than 6 scores
+        public string StudentNames()
+
+        {
+            //specify which text file you would like the program to read
+            StudentData = File.ReadAllLines(@"C:\Users\hogan\Source\Repos\CG-11-1\CG 11-1\CG 11-1\student names.txt");
+
+            //use a loop to go through each line of data and only store and return the names of those that have more than 6 quiz scores
+            foreach (string s in StudentData)
+
+            {
+                string[] score = s.Split(' ');
+                int l = score.Length;
+                if (l > 7)
+                {
+                    Name = score[0];
+                }
+            }
+
+            return Name;
+
+
+
+
+
+        }
+
+    }
+}
